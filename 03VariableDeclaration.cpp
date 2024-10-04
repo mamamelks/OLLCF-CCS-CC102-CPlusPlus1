1ccedaeff638ccf15fce50ae11408b0b1c049653
mamamelks <batiaomelki82@gmail.com>
2024-10-04 09:49:06 +0800

    Add files via upload

---
+++ 03VariableDeclaration.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int age;
+	
+	cout <<"Enter your age\t:";
+	cin >> age;
+	cout <<"Your age is\t;" << age << endl;
+	cout <<"Thank you!"<< endl;
+	
+	return 0;
+}
