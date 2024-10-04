030e2cc0f2499872f40a7650cee86e6b0d0240d8
mamamelks <batiaomelki82@gmail.com>
2024-10-04 09:12:56 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " Our Lady of Lourdes College Foundation " << endl;
+	cout << " College of Computer Studies " << endl;
+	cout << " BSIT 1C " << endl;
+	cout << " Melki Batiao " << endl;
+	cout << " Purok 2, Brgy. Calangcawan Norte, Vinzons, Camarines Norte "<< endl;
+	return 0;
+	
+}
